c7c6a988d52ccdecd73079391013233efbcc1862
Rui Correia <ruiprc@hotmail.com>
2025-07-20 22:31:25 +0100

    base

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <>
+            <h1 className="text-red-800 font-bold">Hello User</h1>
+            <Button>Click me</Button>
+        </>
+    );
+}
